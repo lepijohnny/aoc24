e9b4cb5d3898d5c48adb6333f689487427f1c218
Nikola Radin <lepijohnny@gmail.com>
2024-12-02 22:49:27 +0100

    day03

---
+++ days/day03/part1.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def main() -> None:
+    data = sys.stdin.read().strip()
+    print("...")
+
+
+if __name__ == "__main__":
+    main()
---
+++ days/day03/part2.py
@@ -0,0 +1,9 @@
+import sys
+
+
+def main() -> None:
+    data = sys.stdin.read().strip()
+    print("...")
+
+if __name__ == "__main__":
+    main()
