334fdd703536905cbfe56095306111984c04a224
Nikola Radin <lepijohnny@gmail.com>
2024-12-03 09:39:01 +0100

    day03 sln

--- days/day03/part1.py
+++ days/day03/part1.py
@@ -1,9 +1,22 @@
 import sys
+import re
 
 
 def main() -> None:
     data = sys.stdin.read().strip()
-    print("...")
+
+    pattern = "mul\((?P<a>\d{1,3}),(?P<b>\d{1,3})\)"
+
+    print(data)
+    matches = re.finditer(pattern, data)
+
+    r = 0
+    for match in matches:
+        a = int(match["a"])
+        b = int(match["b"])
+        r += a * b
+
+    print(r)
 
 
 if __name__ == "__main__":
--- days/day03/part2.py
+++ days/day03/part2.py
@@ -1,9 +1,28 @@
 import sys
+import re
 
 
 def main() -> None:
     data = sys.stdin.read().strip()
-    print("...")
+
+    pattern = r"(?P<on>do\(\))|(?P<off>don't\(\))|(?P<mul>mul\((?P<a>\d{1,3}),(?P<b>\d{1,3})\))"
+
+    matches = re.finditer(pattern, data)
+
+    r = 0
+    do = True
+    for match in matches:
+        if match["on"]:
+            do = True
+        if match["off"]:
+            do = False
+        if do and match["mul"]:
+            a = int(match.group("a"))
+            b = int(match.group("b"))
+            r += a * b
+
+    print(r)
+
 
 if __name__ == "__main__":
     main()
