cf3aade885168ad6e0b87352580c975636c8d5c7
Nikola Radin <lepijohnny@gmail.com>
2024-12-04 19:35:54 +0100

    day04 sln

--- days/day04/part1.py
+++ days/day04/part1.py
@@ -1,10 +1,49 @@
-import re
 import sys
+from itertools import repeat
+
+
+def scan(grid: list[str], r: int, c: int) -> int:
+    n, m = len(grid), len(grid[0])
+
+    t = 0
+    for dx, dy in [
+        ([i for i in repeat(r, 4)], [c + i for i in range(4)]),  # r
+        ([i for i in repeat(r, 4)], [c - i for i in range(4)]),  # l
+        ([r - i for i in range(4)], [i for i in repeat(c, 4)]),  # t
+        ([r + i for i in range(4)], [i for i in repeat(c, 4)]),  # b
+        ([r - i for i in range(4)], [c + i for i in range(4)]),  # tr
+        ([r - i for i in range(4)], [c - i for i in range(4)]),  # tl
+        ([r + i for i in range(4)], [c + i for i in range(4)]),  # br
+        ([r + i for i in range(4)], [c - i for i in range(4)]),  # bl
+    ]:
+        if all(0 <= i < n for i in dx) and all(0 <= i < m for i in dy):
+            f = ""
+            for rr, cc in zip(dx, dy):
+                f += grid[rr][cc]
+
+            if f == "XMAS":
+                t += 1
+
+    return t
 
 
 def main() -> None:
     data = sys.stdin.read().strip()
-    print("...")
+
+    grid = []
+    for line in data.split("\n"):
+        grid.append(line)
+
+    n, m = len(grid), len(grid[0])
+
+    t = 0
+    for r in range(n):
+        for c in range(m):
+            if grid[r][c] == "X":
+                t += scan(grid, r, c)
+
+    print(t)
+
 
 if __name__ == "__main__":
     main()
--- days/day04/part2.py
+++ days/day04/part2.py
@@ -1,10 +1,42 @@
-import re
 import sys
+
+
+def scan(grid: list[str], r: int, c: int) -> int:
+    n, m = len(grid), len(grid[0])
+
+    t = 0
+    w = ""
+    for dx, dy in [
+        ([r - 1, r, r + 1], [c - 1, c, c + 1]),  # \
+        ([r + 1, r, r - 1], [c - 1, c, c + 1]),  # /
+    ]:
+        if all(0 <= i < n for i in dx) and all(0 <= i < m for i in dy):
+            for rr, cc in zip(dx, dy):
+                w += grid[rr][cc]
+
+    if w[:3] in ["MAS", "SAM"] and w[3:] in ["MAS", "SAM"]:
+        t += 1
+
+    return t
 
 
 def main() -> None:
     data = sys.stdin.read().strip()
-    print("...")
+
+    grid = []
+    for line in data.split("\n"):
+        grid.append(line)
+
+    n, m = len(grid), len(grid[0])
+
+    t = 0
+    for r in range(n):
+        for c in range(m):
+            if grid[r][c] == "A":
+                t += scan(grid, r, c)
+
+    print(t)
+
 
 if __name__ == "__main__":
     main()
