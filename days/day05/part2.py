faf9649e7515cc48dfc18c41e99170633e0d9f6b
Nikola Radin <lepijohnny@gmail.com>
2024-12-05 08:43:49 +0100

    day05 sln

--- days/day05/part2.py
+++ days/day05/part2.py
@@ -4,7 +4,35 @@ import sys
 
 def main() -> None:
     data = sys.stdin.read().strip()
-    print("...")
+    rules = []
+    orders = []
+
+    br = True
+    for line in data.splitlines():
+        if not line.strip():
+            br = False
+            continue
+
+        if br:
+            p = line.split("|")
+            rules.append((int(p[0]), int(p[1])))
+        else:
+            orders.append(list(map(int, line.split(","))))
+
+    t = 0
+    for order in orders:
+        v = True
+        for i in range(len(order)):
+            for j in range(i + 1, len(order)):
+                if not (order[i], order[j]) in rules:
+                    order[i], order[j] = order[j], order[i]
+                    v = False
+
+        if not v:
+            t += order[len(order) // 2]
+
+    print(t)
+
 
 if __name__ == "__main__":
     main()
