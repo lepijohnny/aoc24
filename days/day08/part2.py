1d127637dafdfa78492a7efe6b0400549a8f7261
Nikola Radin <lepijohnny@gmail.com>
2024-12-08 11:40:40 +0100

    day08.sln.3

--- days/day08/part2.py
+++ days/day08/part2.py
@@ -20,9 +20,9 @@ def main() -> None:
     unq = set()
     for _, v in ants.items():
         for a, b in combinations(v, 2):
-            dx, dy = b[0] - a[0], b[1] - a[1]
-            ax, ay = a[0] - dx, a[1] - dy
-            bx, by = b[0] + dx, b[1] + dy
+            ax, ay = a
+            bx, by = b
+            dx, dy = bx - ax, by - ay
 
             unq.add(a)
             unq.add(b)
@@ -32,6 +32,9 @@ def main() -> None:
                     unq.add((ax, ay))
                     grid[ax][ay] = "#"
                 ax, ay = ax - dx, ay - dy
+
+            ax, ay = a
+            bx, by = b
 
             while bx >= 0 and bx < w and by >= 0 and by < h:
                 if grid[bx][by] not in [ants.keys(), "#"]:
